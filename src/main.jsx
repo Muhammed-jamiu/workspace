3b95f73d5b151491f7d113f4c965fe8b14f5e679
Abdulsalam Muhammed-Jamiu <166193352+Muhammed-jamiu@users.noreply.github.com>
2024-07-11 15:01:09 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import Dashboard from "./Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Dashboard></Dashboard> */}
     <Dashboard />
   </React.StrictMode>
 );
